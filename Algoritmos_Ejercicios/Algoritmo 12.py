6fdd0d15936dc79ccc4a59f1c6d3aa012f4b78a1
SebasTianLoZadaa <lozadajuansebastian931@gmail.com>
2025-05-26 11:36:55 -0500

    Update Algoritmo 12.py

--- Algoritmos_Ejercicios/Algoritmo 12.py
+++ Algoritmos_Ejercicios/Algoritmo 12.py
@@ -1,2 +1,23 @@
-def lista_invertida(lista):
-    return lista[::-1]
+#programa para crear una lista que invierta la primera 
+import random
+
+l = [random.randint (1, 100) for _ in range (20) ]
+print ("lista original:", l)
+
+#crear lista inversa 
+l_inversa = l [:: -1]
+print ("lista inversa :", l_inversa ) 
+
+try:
+    nb= int(input("Ingrese el numero a buscar: "))
+    if nb <1:
+        print ("El numero debe ser mayor a 0")
+    else:
+        if nb in l:
+            p= l.index (nb)
+            print (f"Numero encontrado en la posicion {p}")
+        else:
+         print("Numero no encontrado")
+except ValueError:
+    print("Debe ingresar un numero entero")
+    
